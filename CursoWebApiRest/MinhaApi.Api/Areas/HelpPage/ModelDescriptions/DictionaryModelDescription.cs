771708489be99cce79699a279d03e28ec2a81821
Júnio Moreira da Rocha <junio-moreira@hotmail.com>
2018-12-25 15:45:31 -0200

    Criada a Solução e o projeto base.

---
+++ CursoWebApiRest/MinhaApi.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MinhaApi.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CursoWebApiRest/MinhaApi.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MinhaApi.Api.WebApiApplication" Language="C#" %>
