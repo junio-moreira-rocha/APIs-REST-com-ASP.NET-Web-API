30c04deca7c38ebfe1e10373025ce104f5d40213
Júnio Moreira da Rocha <junio-moreira@hotmail.com>
2019-01-02 11:02:06 -0200

    Adicionada a camada de Acesso a Dados.

---
+++ CursoWebApiRest/MinhaApi.AcessoDados.Entity/Context/MinhaApiDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MinhaApi.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaApi.AcessoDados.Entity.Context
+{
+    public class MinhaApiDbContext : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+
+        public MinhaApiDbContext()
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+    }
+}
