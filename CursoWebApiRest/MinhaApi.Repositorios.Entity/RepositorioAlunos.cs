eb51e00d263fd4edd13a4dd2a5120f1ce492ba29
Júnio Moreira da Rocha <junio-moreira@hotmail.com>
2019-01-02 11:24:49 -0200

    Adicionanda a camada de Repositorios.

---
+++ CursoWebApiRest/MinhaApi.Repositorios.Entity/RepositorioAlunos.cs
@@ -0,0 +1,19 @@
+﻿using MinhaApi.AcessoDados.Entity.Context;
+using MinhaApi.Comum.Repositorios.Entity;
+using MinhaApi.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaApi.Repositorios.Entity
+{
+    public class RepositorioAlunos : RepositorioMinhaApi<Aluno, int>
+    {
+        public RepositorioAlunos(MinhaApiDbContext context)
+            : base(context)
+        {
+        }
+    }
+}
