43a49f06cac0a8eed63d405d0e90f6ed47e3176e
Júnio Moreira da Rocha <junio-moreira@hotmail.com>
2019-01-02 11:03:14 -0200

    Adicionada a camada de Repositorios|Interfaces.

---
+++ CursoWebApiRest/MinhaApi.Comum.Repositorios.Interfaces/IRepositorioMinhaApi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaApi.Comum.Repositorios.Interfaces
+{
+    public interface IRepositorioMinhaApi<TDominio, TChave>
+        where TDominio : class
+    {
+        List<TDominio> Selecionar(Expression<Func<TDominio, bool>> where = null);
+        TDominio SelecionarPorId(TChave id);
+        void Inserir(TDominio dominio);
+        void Atualizar(TDominio dominio);
+        void Excluir(TDominio dominio);
+        void ExcluirPorId(TChave id);
+    }
+}
