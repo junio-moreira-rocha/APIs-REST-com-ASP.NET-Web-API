558f13e8397d9f59d6175d9e3e48f1b190e8b245
Júnio Moreira da Rocha <junio-moreira@hotmail.com>
2019-01-02 15:26:05 -0200

    Criada a Controller para Alunos.

---
+++ CursoWebApiRest/MinhaApi.Api/Controllers/AlunosController.cs
@@ -0,0 +1,28 @@
+﻿using MinhaApi.AcessoDados.Entity.Context;
+using MinhaApi.Comum.Repositorios.Interfaces;
+using MinhaApi.Dominio;
+using MinhaApi.Repositorios.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MinhaApi.Api.Controllers
+{
+    public class AlunosController : ApiController
+    {
+        private IRepositorioMinhaApi<Aluno, int> _repositorioAlunos = new RepositorioAlunos(new MinhaApiDbContext());
+
+        public IEnumerable<Aluno> Get()
+        {
+            return _repositorioAlunos.Selecionar();
+        }
+
+        public Aluno Get(int? id)
+        {
+            return _repositorioAlunos.SelecionarPorId(id.Value);
+        }
+    }
+}
